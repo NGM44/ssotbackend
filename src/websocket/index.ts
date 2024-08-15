8fe09c345a8d6a0ce9eab0a6a4187a45875fe4d5
Sahil Menda <sahilymenda@gmail.com>
2024-08-15 16:21:53 +0530

    process env is not initialized

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -4,21 +4,22 @@ import fs from "fs";
 import { IncomingMessage } from "http";
 
 let wss: WebSocket.Server<typeof WebSocket, typeof IncomingMessage>;
-if (process.env.NODE_ENV === "production") {
-  try {
-    const server = https
-      .createServer({
-        cert: fs.readFileSync("/etc/letsencrypt/live/api.sensormagics.com/fullchain.pem"),
-        key: fs.readFileSync("/etc/letsencrypt/live/api.sensormagics.com/privkey.pem"),
-      })
-      .listen(51001, () => {
-        console.log("HTTPS server listening on port 51001");
-      });
-    wss = new WebSocket.Server({ server });
-  } catch (error) {
-    console.error("Failed to start HTTPS server:", error);
-  }
-} else {
+
+try {
+  const server = https
+    .createServer({
+      cert: fs.readFileSync(
+        "/etc/letsencrypt/live/api.sensormagics.com/fullchain.pem"
+      ),
+      key: fs.readFileSync(
+        "/etc/letsencrypt/live/api.sensormagics.com/privkey.pem"
+      ),
+    })
+    .listen(51001, () => {
+      console.log("HTTPS server listening on port 51001");
+    });
+  wss = new WebSocket.Server({ server });
+} catch (error) {
   try {
     wss = new WebSocket.Server({ port: 8080 });
     console.log("WebSocket server listening on port 8080");
